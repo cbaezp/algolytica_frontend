670e68419f4b0a57cff2ac927f6c740100fdb5db
Cbz <91438023+cbaezp@users.noreply.github.com>
2025-01-14 17:03:03 -0700

    Update Footer.js
    remove commented code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -92,18 +92,6 @@ export default function Footer() {
             </div>
           ))}
         </nav>
-        {/* <div className="mt-10 flex justify-center space-x-10">
-          {navigation.social.map((item) => (
-            <a
-              key={item.name}
-              href={item.href}
-              className="text-gray-400 hover:text-cyan-500"
-            >
-              <span className="sr-only">{item.name}</span>
-              <item.icon className="h-6 w-6" aria-hidden="true" />
-            </a>
-          ))}
-        </div> */}
         <Image
           src="/images/LogoIco.svg"
           width="0"
